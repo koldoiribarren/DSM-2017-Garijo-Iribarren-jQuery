defbabc321f6ae8089f34168ce7acdb8c8b3dc77
Luis Garijo <luisgarijog@gmail.com>
2017-02-17 12:17:23 +0100

    Cambio II,con pestañas nuevas

--- practica2Jquery.js
+++ practica2Jquery.js
@@ -6,4 +6,18 @@ jQuery(document).ready(function(){
 
 	});
 
+
+
+	 $("#content div").hide();
+        $("#tabs li:Precio").attr("id","current");
+        $("#content div:Precio").fadeIn();
+ 
+    $('#tabs a').click(function(e) {
+        e.preventDefault();
+        $("#content div").hide();
+        $("#tabs li").attr("id","");
+        $(this).parent().attr("id","current");
+        $('#' + $(this).attr('title')).fadeIn();
+    });
+
 });
