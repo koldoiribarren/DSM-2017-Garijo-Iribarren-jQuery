472f10fb1d428aa69508279197860db7475356d5
Koldo Iribarren <master@koldoiribarren.com>
2017-03-10 12:05:04 +0100

    javascript arreglado

--- practica2Jquery.js
+++ practica2Jquery.js
@@ -1,19 +1,18 @@
 jQuery(document).ready(function(){
 
 	$("#content div").hide();
-    $("#tabs li:Precio").attr("id","current");
-    $("#content div:Precio").fadeIn();
+    $("#precio").attr("id","current");
+    $("#precio").fadeIn();
  
-    $('#tabs a').click(function()){
+    $('#tabs a').click(function(e){
         e.preventDefault();
         $("#content div").hide();
         $("#tabs li").attr("id","");
         $(this).parent().attr("id","current");
         $('#' + $(this).attr('title')).fadeIn();
-        alert("bullshit");
     });
 
-    $("#Genuino_ZERO_buy").bind('click',function(){
-    
+    $("#Genuino_ZERO_buy").click(function(){
+        
     });
 });
